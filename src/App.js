6c977f88cc0b0a59b78c0846a3118eccdfd35177
dharshanamanoharan <dharshana13@gmail.com>
2024-05-30 11:08:11 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from'react';
 import './App.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Home from './Components/Home';
-
+import About from './Components/About';
+import Product from './Components/Product';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 function App() {
   return (
    <>
