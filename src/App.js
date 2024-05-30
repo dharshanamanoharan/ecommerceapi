f6d53024357e2dd387fff40d9ddeb5082e2859c2
dharshanamanoharan <dharshana13@gmail.com>
2024-05-30 11:06:15 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+
+import React from'react';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './Components/Home';
 
 function App() {
   return (
    <>
+   <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
+    <Footer />
+   </BrowserRouter>
    </>
   );
 }
